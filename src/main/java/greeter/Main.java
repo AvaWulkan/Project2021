cef7b659e2d761962af40f4ce582583bcd9cce08
Ola Lindgård <ola.lindgard@iths.se>
2021-03-30 13:37:54 +0200

    Commit från Ola för att kunna synas på Avas Jenkins.

--- src/main/java/greeter/Main.java
+++ src/main/java/greeter/Main.java
@@ -2,6 +2,8 @@ package greeter;
 
 public class Main {
 
+
+    // Commitar och uppdaterar för att kunna aktivera inlogg till Avas server (Ola).
     public static void main(String[] args) {
         Greeter greeting = new Greeter();
         var x = greeting.greet("Herr Blomberg");
